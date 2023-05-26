a519df5f98674f213bf765009c37d546f57ed7c0
HEITECH\mshafiqrazman <mshafiqrazman@heitech.com.my>
2023-05-26 15:12:41 +0800

    latest

--- index.php
+++ index.php
@@ -1,77 +1,88 @@
 <?php
-
-//if (session_status() !== PHP_SESSION_ACTIVE) {session_start();}
-if(session_id() == '' || !isset($_SESSION)){session_start();}
-
+// if (session_status() !== PHP_SESSION_ACTIVE) {session_start();}
+if (session_id() == '' || !isset($_SESSION)) {
+  session_start();
+}
 ?>
 
 <!DOCTYPE html>
 <html class="no-js" lang="en">
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>BOLT Sports Shop</title>
-    <link rel="stylesheet" href="css/foundation.css" />
-    <script src="js/vendor/modernizr.js"></script>
-  </head>
-  <body>
 
-    <nav class="top-bar" data-topbar role="navigation">
-      <ul class="title-area">
-        <li class="name">
-          <h1><a href="index.php">BOLT Sports Shop</a></h1>
-        </li>
-        <li class="toggle-topbar menu-icon"><a href="#"><span></span></a></li>
-      </ul>
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Art Gallery</title>
+  <link rel="stylesheet" href="css/foundation.css" />
+  <script src="js/vendor/modernizr.js"></script>
+  <style>
+    body {
+      background-image: url("images/front_page.jpg");
+      background-size: cover;
+      background-repeat: no-repeat;
+      background-position: center;
+    }
 
-      <section class="top-bar-section">
+    .art-gallery {
+      text-align: center;
+      padding: 50px 0;
+      color: #fff;
+      text-transform: uppercase;
+      font-size: 48px;
+      /* Updated font size */
+      font-weight: bold;
+      letter-spacing: 2px;
+    }
+  </style>
+</head>
+
+<body>
+
+  <nav class="top-bar" data-topbar role="navigation">
+    <ul class="title-area">
+      <li class="name">
+        <h1><a href="index.php">Art Gallery</a></h1>
+      </li>
+      <li class="toggle-topbar menu-icon"><a href="#"><span></span></a></li>
+    </ul>
+
+    <section class="top-bar-section">
       <!-- Right Nav Section -->
-        <ul class="right">
-          <li><a href="about.php">About</a></li>
-          <li><a href="products.php">Products</a></li>
-          <li><a href="cart.php">View Cart</a></li>
-          <li><a href="orders.php">My Orders</a></li>
-          <li><a href="contact.php">Contact</a></li>
-          <?php
+      <ul class="right">
+        <li><a href="about.php">About</a></li>
+        <li><a href="products.php">Products</a></li>
+        <li><a href="cart.php">View Cart</a></li>
+        <li><a href="orders.php">My Orders</a></li>
+        <li><a href="contact.php">Contact</a></li>
+        <?php
+        if (isset($_SESSION['username'])) {
+          echo '<li><a href="account.php">My Account</a></li>';
+          echo '<li><a href="logout.php">Log Out</a></li>';
+        } else {
+          echo '<li><a href="login.php">Log In</a></li>';
+          echo '<li><a href="register.php">Register</a></li>';
+        }
+        ?>
+      </ul>
+    </section>
+  </nav>
 
-          if(isset($_SESSION['username'])){
-            echo '<li><a href="account.php">My Account</a></li>';
-            echo '<li><a href="logout.php">Log Out</a></li>';
-          }
-          else{
-            echo '<li><a href="login.php">Log In</a></li>';
-            echo '<li><a href="register.php">Register</a></li>';
-          }
-          ?>
-        </ul>
-      </section>
-    </nav>
+  <div class="art-gallery">
+    <p>Art Gallery</p>
+  </div>
 
-
-
-
-    <img data-interchange="[images/bolt-retina.jpg, (retina)], [images/bolt-landscape.jpg, (large)], [images/bolt-mobile.jpg, (mobile)], [images/bolt-landscape.jpg, (medium)]">
-    <noscript><img src="images/bolt-landscape.jpg"></noscript>
-
-
-    <div class="row" style="margin-top:10px;">
-      <div class="small-12">
-
-        <footer style="margin-top:10px;">
-           <p style="text-align:center; font-size:0.8em;">&copy; BOLT Sports Shop. All Rights Reserved.</p>
-        </footer>
-
-      </div>
+  <div class="row" style="margin-top:10px;">
+    <div class="small-12">
+      <!-- <footer style="margin-top:10px;">
+        <p style="text-align:center; font-size:0.8em;">&copy; Art Gallery. All Rights Reserved.</p>
+      </footer> -->
     </div>
+  </div>
 
+  <script src="js/vendor/jquery.js"></script>
+  <script src="js/foundation.min.js"></script>
+  <script>
+    $(document).foundation();
+  </script>
+</body>
 
-
-
-
-    <script src="js/vendor/jquery.js"></script>
-    <script src="js/foundation.min.js"></script>
-    <script>
-      $(document).foundation();
-    </script>
-  </body>
 </html>
